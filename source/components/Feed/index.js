7f0fb8aac05e146e3451c6a0cf864a7ccadaacf3
scann <apple.eater01@gmail.com>
2018-11-19 16:23:10 +0200

    StatusBar component updated with receiving data from context

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -18,12 +18,9 @@ export default class Feed extends Component {
 
         return (
             <section className = { Styles.feed }>
-                <StatusBar { ...this.props }/>
-                <Composer
-                    avatar = { avatar }
-                    currentUserFirstName = { currentUserFirstName }
-                />
-                <Post { ...this.props }/>
+                <StatusBar />
+                <Composer />
+                <Post />
             </section>
         );
     }
