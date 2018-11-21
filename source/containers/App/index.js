bcdf5875c498232ec3e3837b13699ebe380023e3
scann <apple.eater01@gmail.com>
2018-11-21 17:31:52 +0200

    App container refactored - unnecessary HOC declaration removed

--- source/containers/App/index.js
+++ source/containers/App/index.js
@@ -4,7 +4,6 @@ import { hot } from 'react-hot-loader';
 
 //Components
 import Feed from 'components/Feed';
-import { Provider } from 'components/HOC/withProfile';
 
 //Instruments
 import avatar from 'theme/assets/lisa';
@@ -18,10 +17,6 @@ const options = {
 @hot(module)
 export default class App extends Component {
     render() {
-        return (
-            <Provider value = { options }>
-                <Feed />;
-            </Provider>
-        );
+        return <Feed { ...options }/>;
     }
 }
