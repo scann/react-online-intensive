6b936cf5f5c8da750f014c2c0432fad047fec307
scann <apple.eater01@gmail.com>
2018-12-03 16:15:30 +0200

    Component lifecycle methods added

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -1,6 +1,5 @@
 //Core
 import React, { Component } from 'react';
-import moment from 'moment';
 
 //Components
 import { withProfile } from 'components/HOC/withProfile';
@@ -12,36 +11,57 @@ import Catcher from 'components/Catcher';
 
 //Instruments
 import Styles from './styles.m.css';
-import { getUniqueID, delay } from 'instruments';
+import { api, TOKEN } from 'config/api';
 
 @withProfile
 export default class Feed extends Component {
-
     state = {
-        posts: [
-            { id: '123', comment: 'Hi there!', created: Object(1526825076849), likes: [] },
-            { id: '456', comment: 'Hello!', created: Object(1526825076855), likes: [] },
-        ],
+        posts:      [],
         isSpinning: false,
     };
+
+    componentDidMount () {
+        this._fetchPosts();
+        this.refetch = setInterval(this._fetchPosts(), 1000);
+    }
+
+    componentWillUnmout() {
+        clearInterval(this.refetch);
+    }
 
     _setPostsFetchingState = (state) => {
         this.setState({
             isSpinning: state,
         });
-    }
+    };
+
+    _fetchPosts = async () => {
+        this._setPostsFetchingState(true);
+
+        const response = await fetch(api, {
+            method: 'GET',
+        });
+
+        const { data: posts } = await response.json();
+
+        this.setState({
+            posts,
+            isSpinning: false,
+        });
+    };
 
     _createPost = async (comment) => {
         this._setPostsFetchingState(true);
 
-        const post = {
-            id:      getUniqueID(),
-            created: moment.utc(),
-            comment,
-            likes:   [],
-        };
-
-        await delay(1200);
+        const response = await fetch(api, {
+            method:  'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization:  TOKEN,
+            },
+            body: JSON.stringify({ comment }),
+        });
+        const { data: post } = await response.json();
 
         this.setState(({ posts }) => ({
             posts:      [ post, ...posts ],
@@ -52,7 +72,12 @@ export default class Feed extends Component {
     _removePost = async (id) =>  {
         this._setPostsFetchingState(true);
 
-        await delay(1200);
+        await fetch(`${api}/${id}`, {
+            method:  'DELETE',
+            headers: {
+                Authorization: TOKEN,
+            },
+        });
 
         this.setState(({ posts }) => ({
             posts:      posts.filter((post) => post.id !== id),
@@ -61,34 +86,24 @@ export default class Feed extends Component {
     }
 
     _likePost = async (id) => {
-        const { currentUserFirstName, currentUserLastName } = this.props;
-
         this._setPostsFetchingState(true);
 
-        await delay(1200);
-
-        const newPosts = this.state.posts.map((post) => {
-            if (post.id === id) {
-                return {
-                    ...post,
-                    likes: [
-                        {
-                            id:        getUniqueID(),
-                            firstName: currentUserFirstName,
-                            lastName:  currentUserLastName,
-                        },
-                    ],
-                };
-            }
-
-            return post;
+        const response = await fetch(`${api}/${id}`, {
+            method:  'PUT',
+            headers: {
+                Authorization: TOKEN,
+            },
         });
 
-        this.setState({
-            posts:      newPosts,
+        const { data: likedPost } = await response.json();
+
+        this.setState(({ posts }) => ({
+            posts: posts.map(
+                (post) => post.id === likedPost.id ? likedPost : post,
+            ),
             isSpinning: false,
-        });
-    }
+        }));
+    };
 
     render() {
         const { posts, isSpinning } = this.state;
