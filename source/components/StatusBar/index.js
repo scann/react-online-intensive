949dfcdb527981cf551452f753e71e074f85d222
scann <apple.eater01@gmail.com>
2018-12-07 11:52:29 +0200

    StatusBar component - opacity animation added

--- source/components/StatusBar/index.js
+++ source/components/StatusBar/index.js
@@ -1,6 +1,9 @@
 //Core
 import React, { Component } from 'react';
 import cx from 'classnames';
+import { Transition } from 'react-transition-group';
+import { fromTo } from 'gsap';
+
 
 //Components
 import { withProfile } from 'components/HOC/withProfile';
@@ -34,6 +37,10 @@ export default class StatusBar extends Component {
         socket.removeListener('disconnect');
     }
 
+    _animateStatusBarEnter = (statusBar) => {
+        fromTo(statusBar, 1, { opacity: 0 }, { opacity: 1 });
+    };
+
     render() {
         const {
             avatar,
@@ -50,18 +57,24 @@ export default class StatusBar extends Component {
         const statusMessage = online ? 'Online' : 'Offline';
 
         return (
-            <section className = { Styles.statusBar }>
-                <div className = { statusStyle }>
-                    <div>{ statusMessage }</div>
-                    <span />
-                </div>
-                <button>
-                    <img src = { avatar } />
-                    <span>{currentUserFirstName}</span>
-                            &nbsp;
-                    <span>{currentUserLastName}</span>
-                </button>
-            </section>
+            <Transition
+                appear
+                in
+                timeout = { 1000 }
+                onEnter = { this._animateStatusBarEnter }>
+                <section className = { Styles.statusBar }>
+                    <div className = { statusStyle }>
+                        <div>{ statusMessage }</div>
+                        <span />
+                    </div>
+                    <button>
+                        <img src = { avatar } />
+                        <span>{currentUserFirstName}</span>
+                        &nbsp;
+                        <span>{currentUserLastName}</span>
+                    </button>
+                </section>
+            </Transition>
         );
     }
 }
