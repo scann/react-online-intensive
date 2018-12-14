2ad772f3d982725b190fa47336f137b1736fb061
scann <apple.eater01@gmail.com>
2018-12-14 20:52:03 +0200

    Hometask - tests for getFullApiUrl function

--- source/instruments/index.test.js
+++ source/instruments/index.test.js
@@ -1,5 +1,5 @@
 //Core
-import { sum, delay, getUniqueID } from './';
+import { sum, delay, getUniqueID, getFullApiUrl } from './';
 
 jest.setTimeout(10000);
 
@@ -21,7 +21,7 @@ describe('instruments:', () => {
     test('delay function should return a resolved promise', async () => {
         await expect(delay(5000)).resolves.toBeUndefined();
     });
-    test('getUniqueID shouldbe a function', () => {
+    test('getUniqueID should be a function', () => {
         expect(getUniqueID).toBeInstanceOf(Function);
     });
     test('getUniqueID function should throw, when called with non-number type as an argument', () => {
@@ -31,6 +31,25 @@ describe('instruments:', () => {
         expect(typeof getUniqueID()).toBe('string');
         expect(getUniqueID(5)).toHaveLength(5);
         expect(getUniqueID(13)).toHaveLength(13);
+    });
+    test('getFullApiUrl should be a function', () => {
+        expect(getFullApiUrl).toBeInstanceOf(Function);
+    });
+    test('getFullApiUrl function should throw, when called with non-string type as first argument', () => {
+        expect(() => getFullApiUrl(5, 'hello')).toThrow();
+    });
+    test('getFullApiUrl function should throw, when called with non-string type as second argument', () => {
+        expect(() => getFullApiUrl('hello', 5)).toThrow();
+    });
+    test('getFullApiUrl function should return a string value', () => {
+        expect(typeof getFullApiUrl('hello', 'world')).toBe('string');
+    });
+    test('getFullApiUrl function should return a concatenated string', () => {
+        expect(getFullApiUrl('hello', 'world')).toBe('hello/world');
+    });
+    test('getFullApiUrl function should return a concatenated string with slash symbol', () => {
+        //expect(() => getFullApiUrl('hello', 'world')).stringContaining('/');
+        expect(getFullApiUrl('hello', 'world')).toEqual(expect.stringContaining('/'));
     });
 });
 
