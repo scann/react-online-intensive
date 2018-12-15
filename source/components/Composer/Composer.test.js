5b3e96a66a38fe6834cee8ac28ee920171f9251b
scann <apple.eater01@gmail.com>
2018-12-15 20:59:43 +0200

    Hometask - Composer component - tests for avatar and currentUserFirstName props and also for _updateComment and _submitOnEnter methods

--- source/components/Composer/Composer.test.js
+++ source/components/Composer/Composer.test.js
@@ -4,7 +4,9 @@ import { mount } from 'enzyme';
 import { Composer } from './';
 
 const props = {
-    _createPost: jest.fn(),
+    _createPost:          jest.fn(),
+    avatar:               'img/avatar.jpg',
+    currentUserFirstName: 'Anna',
 };
 
 const comment = 'Merry Christmas!';
@@ -21,6 +23,8 @@ const result = mount(<Composer { ...props } />);
 
 const _submitCommentSpy = jest.spyOn(result.instance(), '_submitComment');
 const _handleFormSubmitSpy = jest.spyOn(result.instance(), '_handleFormSubmit');
+const _updateCommentSpy = jest.spyOn(result.instance(), '_updateComment');
+const _submitOnEnterSpy = jest.spyOn(result.instance(), '_submitOnEnter');
 
 describe('composer component:', () => {
     test('should have 1 "section" element', () => {
@@ -91,6 +95,29 @@ describe('composer component:', () => {
     test('_submitComment and _handleFormSubmit class methods should be invoked once after form is submitted', () => {
         expect(_submitCommentSpy).toHaveBeenCalledTimes(1);
         expect(_handleFormSubmitSpy).toHaveBeenCalledTimes(1);
+    });
+
+    test('avatar prop should contain a string value', () => {
+        expect(props.avatar).toBe('img/avatar.jpg');
+    });
+
+    test('currentUserFirstName prop should contain a string value', () => {
+        expect(props.currentUserFirstName).toBe('Anna');
+    });
+
+    test('_updateComment class method should be invoked once after cursor is placed in textarea', () => {
+        result.find('textarea').simulate('focus', {
+            target: {
+                value: '',
+            },
+        });
+        expect(result.state()).toEqual(initialState);
+        expect(_updateCommentSpy).toHaveBeenCalledTimes(1);
+    });
+
+    test('_submitOnEnter class method should be invoked once when user pressed Enter key in textarea', () => {
+        result.find('textarea').simulate('keypress', { key: 'Enter' });
+        expect(_submitOnEnterSpy).toHaveBeenCalledTimes(1);
     });
 });
 
