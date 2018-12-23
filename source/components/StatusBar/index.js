6c41bb117490c9921215d871b488e5e141832314
scann <apple.eater01@gmail.com>
2018-12-23 15:03:49 +0200

    StatusBar component - routing link is used instead of button

--- source/components/StatusBar/index.js
+++ source/components/StatusBar/index.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import cx from 'classnames';
 import { Transition } from 'react-transition-group';
 import { fromTo } from 'gsap';
+import { Link } from 'react-router-dom';
 
 
 //Components
@@ -67,12 +68,11 @@ export default class StatusBar extends Component {
                         <div>{ statusMessage }</div>
                         <span />
                     </div>
-                    <button>
+                    <Link to = '/profile'>
                         <img src = { avatar } />
                         <span>{currentUserFirstName}</span>
-                        &nbsp;
-                        <span>{currentUserLastName}</span>
-                    </button>
+                    </Link>
+                    <Link to = '/feed'>Feed</Link>
                 </section>
             </Transition>
         );
