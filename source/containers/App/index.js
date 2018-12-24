322584902bf9b88a204a12981a2f2cbde0340b73
scann <apple.eater01@gmail.com>
2018-12-24 20:54:00 +0200

    Homework - App container - isAuthenticated field, toggleLogin method added, state updated

--- source/containers/App/index.js
+++ source/containers/App/index.js
@@ -8,34 +8,61 @@ import Catcher from 'components/Catcher';
 import StatusBar from 'components/StatusBar';
 import Feed from 'components/Feed';
 import Profile from 'components/Profile';
+import Login from 'components/Login';
 import { Provider } from 'components/HOC/withProfile';
 
 //Instruments
 import avatar from 'theme/assets/lisa';
 
-const options = {
-    avatar,
-    currentUserFirstName: 'Анна',
-    currentUserLastName:  'Краснощек',
-};
-
 @hot(module)
 export default class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this._toggleLogin = (state) => {
+            this.setState(() => ({
+                isAuthenticated: state,
+            }));
+        };
+
+        this.state = {
+            avatar,
+            currentUserFirstName: 'Анна',
+            currentUserLastName:  'Краснощек',
+            isAuthenticated:      false,
+            _toggleLogin:         this._toggleLogin,
+        };
+    }
+
+    componentDidMount() {
+        const login = window.localStorage.getItem('fb_login');
+        if (login) {
+            this._toggleLogin(login);
+        }
+    }
+
     render() {
         return (
             <Catcher>
-                <Provider value = { options }>
+                <Provider value = { this.state }>
                     <StatusBar />
                     <Switch>
-                        <Route
+                        { this.state.isAuthenticated
+                        && <Route
                             component = { Feed }
                             path = '/feed'
-                        />
-                        <Route
+                           /> }
+                        { this.state.isAuthenticated
+                        && <Route
                             component = { Profile }
                             path = '/profile'
+                           /> }
+                        <Route
+                            _toggleLogin = { this.state._toggleLogin }
+                            component = { Login }
+                            path = '/login'
                         />
-                        <Redirect to = '/feed'/>
+                        <Redirect to = '/login'/>
                     </Switch>
                 </Provider>
             </Catcher>
